83ceb8d17c4f2bbdca464bc0c4b355e02ead8116
satchapum <satchapum.mail@mail.kmutt.ac.th>
2024-01-31 23:45:23 +0700

    ADD:LAB3

--- Assets/Script/LoginManagerScript.cs
+++ Assets/Script/LoginManagerScript.cs
@@ -10,15 +10,21 @@ using Unity.Mathematics;
 
 public class LoginManagerScript : MonoBehaviour
 {
-    public TMP_InputField characterIdInputField;
     public List<uint> AlternativePlayerPrefabs;
+    public TMP_Dropdown dropdown_TMP;
 
     public TMP_InputField userNameInputField;
+    public TMP_InputField roomIdInputField;
     private bool isApproveConnection = false;
     [Command("set-approve")]
 
     public GameObject loginPanel;
     public GameObject leaveButton;
+
+    [Header("SpawnPos")]
+    [SerializeField] Transform[] posList;
+
+    public int roomID;
 
     private void Start()
     {
@@ -44,7 +50,7 @@ public class LoginManagerScript : MonoBehaviour
 
         }
 
-        else if (NetworkManager.Singleton.IsClient)
+        else if (NetworkManager.Singleton.IsHost)
         {
             NetworkManager.Singleton.Shutdown();
         }
@@ -85,6 +91,8 @@ public class LoginManagerScript : MonoBehaviour
     {
         NetworkManager.Singleton.ConnectionApprovalCallback = ApprovalCheck;
         NetworkManager.Singleton.StartHost();
+        roomID = int.Parse(roomIdInputField.GetComponent<TMP_InputField>().text);
+        Debug.Log("Room id is: " + roomID);
         Debug.Log("Start host");
     }
 
@@ -99,30 +107,60 @@ public class LoginManagerScript : MonoBehaviour
         int byteLength = connectionData.Length;
         bool isApprove = false;
         int characterPrefabIndex = 0;
+
+        bool nameCheck = false;
+        bool roomIDCheck = false;
         if (byteLength > 0)
         {
             string combinedString = System.Text.Encoding.ASCII.GetString(connectionData,0,byteLength);
             string[] extractedString = HelperScript.ExtractStrings(combinedString);
+
+            string hostData = userNameInputField.GetComponent<TMP_InputField>().text;
+            nameCheck = NameApproveConnection(extractedString[0], hostData);
+            roomIDCheck = RoomIDApproveConnection(extractedString[2]);
+
             for (int i = 0; i < extractedString.Length; i++)
             {
                 if (i == 0)
                 {
                     string clientData = extractedString[i];
-                    string hostData = userNameInputField.GetComponent<TMP_InputField>().text;
-                    isApprove = ApproveConnection(clientData, hostData);
+                    isApprove = NameApproveConnection(clientData, hostData);
+                    if ((nameCheck == true) && (roomIDCheck == true))
+                    {
+                        isApprove = true;
+                    }
+                    else if ((nameCheck == false) && (roomIDCheck == false))
+                    {
+                        isApprove = false;
+                        Debug.Log("Name is use alr && Room id is not correct");
+                    }
+                    else if ((nameCheck == false) || (roomIDCheck == false))
+                    {
+                        if (nameCheck == false)
+                        {
+                            isApprove = false;
+                            Debug.Log("Name is use alr");
+                        }
+                        else
+                        {
+                            isApprove = false;
+                            Debug.Log("Room id is not correct");
+                        }
+                    }
                 }
-                else
+                else if (i == 1)
                 {
                     characterPrefabIndex = int.Parse(extractedString[i]);
                 }
             }
         }
+
         else
         {
             //server
             if (NetworkManager.Singleton.IsHost)
             {
-                string characterId = characterIdInputField.GetComponent<TMP_InputField>().text;
+                string characterId = setInputSkinData().ToString();
                 characterPrefabIndex = int.Parse(characterId);
             }
         }
@@ -138,14 +176,13 @@ public class LoginManagerScript : MonoBehaviour
         // Position to spawn the player object (if null it uses default of Vector3.zero)
         response.Position = Vector3.zero;
 
-        NetworkLog.LogInfoServer("SpanwnPos of " + clientId + " is " + response.Position.ToString());
         // Rotation to spawn the player object (if null it uses the default of Quaternion.identity)
 
         response.Rotation = Quaternion.identity;
 
-        NetworkLog.LogInfoServer("SpanwnRot of " + clientId + " is " + response.Rotation.ToString());
-
         SetSpawnLocation(clientId, response);
+        NetworkLog.LogInfoServer("SpanwnPos of " + clientId + " is " + response.Position.ToString());
+        NetworkLog.LogInfoServer("SpanwnRot of " + clientId + " is " + response.Rotation.ToString());
 
         // If response.Approved is false, you can provide a message that explains the reason why via ConnectionApprovalResponse.Reason
         // On the client-side, NetworkManager.DisconnectReason will be populated with this message via DisconnectReasonMessage
@@ -163,12 +200,20 @@ public class LoginManagerScript : MonoBehaviour
         //server
         if(clientId == NetworkManager.Singleton.LocalClientId)
         {
-            spawnPos = new Vector3(-2f, 0f, 0f);
-            spawnRot = Quaternion.Euler(0f, 135f, 0f);
+            int countOfAllPos = posList.Length;
+            int randomPos = UnityEngine.Random.Range(0, countOfAllPos);
+            Debug.Log("Random pos is : " + randomPos);
+            spawnPos = new Vector3(posList[randomPos].position.x, posList[randomPos].position.y, posList[randomPos].position.z);
+            spawnRot = Quaternion.Euler(posList[randomPos].eulerAngles.x, posList[randomPos].eulerAngles.y, posList[randomPos].eulerAngles.z);
         }
         else
         {
-            switch(NetworkManager.Singleton.ConnectedClients.Count)
+            int countOfAllPos = posList.Length;
+            int randomPos = UnityEngine.Random.Range(0, countOfAllPos);
+            Debug.Log("Random pos is : " + randomPos);
+            spawnPos = new Vector3(posList[randomPos].position.x, posList[randomPos].position.y, posList[randomPos].position.z);
+            spawnRot = Quaternion.Euler(posList[randomPos].eulerAngles.x, posList[randomPos].eulerAngles.y, posList[randomPos].eulerAngles.z);
+            /*switch(NetworkManager.Singleton.ConnectedClients.Count)
             {
                 case 1:
                     spawnPos = new Vector3(0f, 0f, 0f);
@@ -178,7 +223,7 @@ public class LoginManagerScript : MonoBehaviour
                     spawnPos = new Vector3(2f, 0f, 0f);
                     spawnRot = Quaternion.Euler(0f,225f, 0f);
                     break;
-            }
+            }*/
         }
         response.Position = spawnPos;
         response.Rotation = spawnRot;
@@ -187,18 +232,49 @@ public class LoginManagerScript : MonoBehaviour
     public void Client()
     {
         string username = userNameInputField.GetComponent<TMP_InputField>().text;
-        string characterId = characterIdInputField.GetComponent<TMP_InputField>().text;
-        string[] inputFields = { username, characterId };
+        string characterId = setInputSkinData().ToString();
+        string roomID = roomIdInputField.GetComponent<TMP_InputField>().text;
+        string[] inputFields = { username, characterId, roomID };
         string clientData = HelperScript.CombineStrings(inputFields);
         NetworkManager.Singleton.NetworkConfig.ConnectionData = System.Text.Encoding.ASCII.GetBytes(clientData);
         NetworkManager.Singleton.StartClient();
         Debug.Log("Start client");
+        
     }
 
-    public bool ApproveConnection(string clientData, string hostData)
+        public bool NameApproveConnection(string clientData, string hostData)
     {
         bool isApprove = System.String.Equals(clientData.Trim(), hostData.Trim()) ? false : true;
-        Debug.Log("isApprove = " + isApprove);
+        Debug.Log("NameIsApprove = " + isApprove);
         return isApprove;
+    }
+
+    public bool RoomIDApproveConnection(string clientData)
+    {
+        bool isApprove;
+        if (int.Parse(clientData) == roomID)
+        {
+            isApprove = true;
+        }
+        else
+        {
+            isApprove = false;
+        }
+        Debug.Log("RoomIDIsApprove = " + isApprove);
+        return isApprove;
+    }
+
+    public int setInputSkinData()
+    {
+
+        if (dropdown_TMP.GetComponent<TMP_Dropdown>().value == 0)
+        {
+            return 0;
+        }
+        if (dropdown_TMP.GetComponent<TMP_Dropdown>().value == 1)
+        {
+            return 1;
+        }
+        return 0;
     }
 }
